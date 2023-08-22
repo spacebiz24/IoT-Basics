07163d35f9bfe1f2e6c20ce532305acd80dbfaec
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-08-23 01:32:51 +0530

    Create P1.py

---
+++ P1.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GP
+import time as T
+
+LED_PIN = 18
+
+GP.setmode(GP.BCM)
+GP.setup(LED_PIN, GP.OUT)
+try:
+  while True:
+    GP.output(LED_PIN, True)
+    T.sleep(2)
+    GP.output(LED_PIN, False)
+    T.sleep(2)
+except KeyboardInterrupt:
+  GP.cleanup()
+  exit()
