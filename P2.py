739d8d4a77c44d6d9967bae4ec4a25384bbd5ef2
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-08-23 10:17:09 +0530

    Update P2.py

--- P2.py
+++ P2.py
@@ -13,6 +13,6 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 # Main
 while True:
     if GPIO.input(LDR_PIN):
-      GPIO.output(LED, 1)
+      GPIO.output(LED_PIN, 1)
     else:
-      GPIO.output(LED, 0)
+      GPIO.output(LED_PIN, 0)
