88cefac0c20c5bf2cd62c3a204dd7fdca74f9bec
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-08-23 21:39:18 +0530

    Create P2.py

---
+++ P2/P2.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+# Pi Setup
+GPIO.setwarnings(0)
+GPIO.setmode(GPIO.BCM)
+
+# GPIO Setup
+LDR_PIN = 22
+GPIO.setup(LDR_PIN, GPIO.IN)
+LED_PIN = 18
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+# Main
+while True:
+    if GPIO.input(LDR_PIN):
+      GPIO.output(LED_PIN, 1)
+    else:
+      GPIO.output(LED_PIN, 0)
