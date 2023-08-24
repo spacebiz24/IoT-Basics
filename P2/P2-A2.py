994ef65c51743413051849b32af60af75fbc4bac
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-08-24 14:46:45 +0530

    Create P2-A2.py

---
+++ P2/P2-A2.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pi Setup
+GPIO.setwarnings(0)
+GPIO.setmode(GPIO.BCM)
+
+# GPIO Setup
+LDR_PIN = 22
+GPIO.setup(LDR_PIN, GPIO.IN)
+LED = [18,21]
+GPIO.setup(LED, GPIO.OUT)
+
+# Main
+while True:
+    if GPIO.input(LDR_PIN):
+      GPIO.output(LED[0], 1)
+      time.sleep(1)
+      GPIO.output(LED[1], 1)
+    else:
+      GPIO.output(LED[0], 0)
+      time.sleep(0.5)
+      GPIO.output(LED[1], 0)
