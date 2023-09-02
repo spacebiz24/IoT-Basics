2a4281b22855395139bf6bda3235e7684d5f53b3
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-09-02 23:25:37 +0530

    Create P4.py

---
+++ P4/P4.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+RELAY_PIN_1 = 22
+RELAY_PIN_2 = 27
+
+GPIO.setup(RELAY_PIN_1, GPIO.OUT)
+GPIO.setup(RELAY_PIN_2, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(RELAY_PIN_1, True)
+        GPIO.output(RELAY_PIN_2, True)
+        time.sleep(4)
+        GPIO.output(RELAY_PIN_1, False)
+        GPIO.output(RELAY_PIN_2, False)
+        time.sleep(4)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    exit()
