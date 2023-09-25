e6c217961163d7ec38062a4d68a34236b01c6609
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-09-25 12:41:03 +0530

    Update P4-A1.py

--- P4/P4-A1.py
+++ P4/P4-A1.py
@@ -7,18 +7,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 # Pin Setup
-RELAY_PIN_1 = 22
-RELAY_PIN_2 = 27
-GPIO.setup(RELAY_PIN_1, GPIO.OUT)
-GPIO.setup(RELAY_PIN_2, GPIO.OUT)
+RELAY_PINS = [22, 27]
+GPIO.setup(RELAY_PINS, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(RELAY_PIN_1, True)
-        GPIO.output(RELAY_PIN_2, True)
+        GPIO.output(RELAY_PINS, True)
         time.sleep(4)
-        GPIO.output(RELAY_PIN_1, False)
-        GPIO.output(RELAY_PIN_2, False)
+        GPIO.output(RELAY_PINS, False)
         time.sleep(4)
 except KeyboardInterrupt:
     GPIO.cleanup()
