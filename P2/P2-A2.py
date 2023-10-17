4bbf0195389544a5026d1ab634ebbc5b16277be0
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-10-17 20:19:49 +0530

    Update P2-A2.py

--- P2/P2-A2.py
+++ P2/P2-A2.py
@@ -15,8 +15,8 @@ GPIO.setup(LED, GPIO.OUT)
 
 # Main
 while True:
-    ldr_val = GPIO.input(LDR_PIN)
-    delay = 1 if ldr_val else 0.5
-    GPIO.output(LED[0],ldr_val)
+    LDR_Value = GPIO.input(LDR_PIN)
+    Delay = 1 if LDR_Value else 0.5
+    GPIO.output(LED[0], LDR_Value)
     time.sleep(delay)
-    GPIO.output(LED[1],ldr_val)
+    GPIO.output(LED[1], LDR_Value)
