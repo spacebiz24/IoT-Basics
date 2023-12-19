6447264988e6e593c2e187d73f056bd49a4104d6
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-12-19 08:00:20 +0530

    Update P6-Client.py

--- P6/P6-Client.py
+++ P6/P6-Client.py
@@ -5,10 +5,9 @@ localPort = 20001
 bufferSize = 1024
 
 messageFromClient = "Hello UDP Server"
-serverAddressPort = (localIP, localPort)
 BytesToSend = str.encode(messageFromClient)
 ClientSocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
-ClientSocket.sendto(BytesToSend, serverAddressPort)
+ClientSocket.sendto(BytesToSend, (localIP, localPort))
 
 serverMessage = ClientSocket.recvfrom(bufferSize)
 print(f"Message From Server: {serverMessage[0]}")
